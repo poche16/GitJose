d773e6d5080e147521dd99c8f4f58f95e4e3130f
Jose <4173369@alu.murciaeduca.es>
2025-05-12 10:02:22 +0200

    Primer commit del Ejercicio 2

---
+++ Entrenador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ut5;
+
+/**
+ *
+ * @author daw
+ */
+public class Entrenador extends Persona {
+    private String idFederacion;
+    public Entrenador(int id, String nombre, String apellidos, int edad) {
+        super(id, nombre, apellidos, edad);
+    }
+    
+}
---
+++ Futbolista.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ut5;
+
+/**
+ *
+ * @author daw
+ */
+public class Futbolista extends Persona{
+    private int dorsal;
+    private String demarcacion;
+    public Futbolista (int id, String nombre, String apellidos, int edad){
+        super(id, nombre, apellidos, edad);
+    }
+
+}
